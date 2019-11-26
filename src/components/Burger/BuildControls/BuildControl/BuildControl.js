3aa06610d7dd306911b51ca8e1a451aad86f787f
sandroalmeida <sandro.almeida@dotbuild.com.br>
2019-11-25 16:03:02 -0800

    Added Build Controls components.

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import classes from './BuildControl.css';
+import classes from './BuildControl.module.css';
 
-const buildControl = (props) => {
+const buildControl = (props) => (
     <div className={classes.BuildControl}>
-        <div className={classes.label}>{props.label}</div>
+        <div className={classes.Label}>{props.label}</div>
         <button className={classes.Less}>Less</button>
         <button className={classes.More}>More</button>
     </div>
 
-};
+);
 
 export default buildControl;
